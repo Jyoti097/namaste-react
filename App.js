81621fdddbc724ddaa395a883897d5c1dda69529
Jyoti Ola <olajyoti0483@gmail.com>
2024-01-27 22:00:11 +0530

    Episode-01

---
+++ App.js
@@ -0,0 +1,41 @@
+//Creating nested HTML by React
+{/* <div id = "parent">
+    <div id = "child">
+        <h1>I'm h1 tag</h1>
+    </div>
+</div> */}
+
+const parent1 = React.createElement(
+    "div", 
+    {id:"parent"},
+    React.createElement(
+        "div", 
+        {id:"child"},
+        React.createElement("h1", {}, "I'm a h1 tag")
+    )
+);
+
+
+//const heading = React.createElement("h1", {id : "heading"}, "Hello from React");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent1);
+
+// creating siblings 
+{/* <div id = "parent">
+    <div id = "child">
+        <h1>I'm h1 tag</h1>
+        <h2>I'm h2 tag</h2>
+    </div>
+</div> */}
+
+const parent2 = React.createElement(
+    "div", 
+    {id:"parent"},
+    React.createElement(
+        "div", 
+        {id:"child"},
+        [React.createElement("h1", {}, "I'm a h1 tag"), React.createElement("h2", {}, "I'm a h2 tag")]
+    )
+);
+const root2 = ReactDOM.createRoot(document.getElementById("root"));
+root2.render(parent2);
